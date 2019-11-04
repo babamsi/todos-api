10ef3a60c356907a4bf40cc961157e84ecd553bc
suhaybcabdulahi <sabkacade40@gmail.com>
2019-11-03 19:39:00 -0500

    Update security issues

--- servers/db/mongoose.js
+++ servers/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = { mongoose }
